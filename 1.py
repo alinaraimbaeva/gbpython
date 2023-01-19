f838d53f6d27d0710cfc1593a2e92e4e66716906
alinaraimbaeva <raimbaeva@gmail.com>
2023-01-19 03:45:43 +0300

    1 task

---
+++ 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 18:02:08 2023
+
+@author: alinaraimbaeva
+"""
+'''1. Поработайте с переменными, создайте несколько, выведите на экран. 
+Запросите у пользователя некоторые числа и 
+строки и сохраните в переменные, затем выведите на экран.'''
+
+name= input('My name: ')
+land=input('Homeland: ')
+age=input('Age: ')
+print(f"I'm {name}, I came from {land} and I'm {age} years old.")
